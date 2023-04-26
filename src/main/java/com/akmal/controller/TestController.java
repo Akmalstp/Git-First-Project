48b675592827df4272fe93e6dfe9f617b27520f7
Akmalstp <diagovenk@gmail.com>
2023-04-26 21:40:14 +0530

    Modified Testcontroller, add test endpoint

--- src/main/java/com/akmal/controller/TestController.java
+++ src/main/java/com/akmal/controller/TestController.java
@@ -1,8 +1,13 @@
 package com.akmal.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 	
+	@PostMapping("\testPath")
+	public String testMethod() {	
+		return "";
+	}
 }
